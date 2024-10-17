fbbeed1a444f1652d3cbd2619a4b59cdcafe9442
Jack Sari <janasarii@gmail.com>
2024-10-16 19:52:38 -0500

    add

---
+++ src/app/404.tsx
@@ -0,0 +1,11 @@
+const Custom404 = () => {
+    return (
+      <div>
+        <h1>Página No Encontrada</h1>
+        <p>Lo sentimos, la página que buscas no existe.</p>
+      </div>
+    );
+  };
+  
+  export default Custom404;
+  
